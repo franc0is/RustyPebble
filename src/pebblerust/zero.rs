985aef6658eb87c9f92cadf032f2556f95888c82
Marcel Jackwerth <marceljackwerth@gmail.com>
2015-02-22 16:02:34 +0100

    Added copyright notice in zero.rs

--- src/pebblerust/zero.rs
+++ src/pebblerust/zero.rs
@@ -1,35 +1,28 @@
+// Copyright 2013-2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+//
+// Copied from
+// https://github.com/rust-lang/rust/blob/master/src/test/run-pass/smallest-hello-world.rs
+
 #![feature(intrinsics)]
 
-extern crate core;
+extern { fn puts(s: *const u8); }
+extern "rust-intrinsic" { fn transmute<T, U>(t: T) -> U; }
 
-use pebblerust::c;
+#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
 
-extern "rust-intrinsic" {
-    pub fn transmute<T,U>(val: T) -> U;
-}
-
-#[lang="stack_exhausted"] extern fn stack_exhausted() {}
-
-#[lang="eh_personality"] extern fn eh_personality() {}
-
-#[lang="panic_fmt"]
-pub fn panic_fmt(_fmt: &core::fmt::Arguments, _file_line: &(&'static str, usize)) -> ! {
-  // TODO: logging
-
-  unsafe {
-    c::window_stack_pop_all();
-  }
-
-  loop {}
-}
+// Additions
 
 // If we don't implement this here the linker will link against libunwind
 // which will then require kill, getpid()... which isn't available.
 #[no_mangle]
-pub unsafe fn __aeabi_unwind_cpp_pr0() -> () {
-  unsafe {
-    c::window_stack_pop_all();
-  }
-
-  loop {}
-}
+pub unsafe fn __aeabi_unwind_cpp_pr0() -> () { loop {} }
